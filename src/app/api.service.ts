92cdc239517df036005db1fe2e80bfb0f0b213fa
Tinh Pham <phamvantinhk37@gmail.com>
2020-02-05 22:28:44 +0700

    the first commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private PRODUCT_API = 'http://localhost:3000/products';
+  constructor(private httpClient: HttpClient) { }
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    // window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  public sendGetRequest() {
+    return this.httpClient.get(this.PRODUCT_API).pipe(catchError(this.handleError));
+  }
+}
